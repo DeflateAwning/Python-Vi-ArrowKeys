82b07c7b4ba089b3ffff4685c2685ab38030f63e
ThePiGuy <ThePiGuy@users.noreply.github.com>
2020-04-30 13:13:09 -0600

    Fix 'cards' bug, improve documentation

--- Python-Vi-ArrowKeys.py
+++ Python-Vi-ArrowKeys.py
@@ -8,7 +8,8 @@ gstate = { # global state of the system
 	"down": set(), # set of characters currently pressed (set bc there will only ever be a single instance of each)
 	"lastInfo": "", # stores an information string printed to the user, for caching
 	"lastInfoCount": 0,
-	"viTriggeredYet": False # whether VI mode has been triggered while d has been pressed (decides where or not to type a 'd' on 'd UP')
+	"viTriggeredYet": False, # whether VI mode has been triggered while d has been pressed (decides where or not to type a 'd' on 'd UP')
+	"dSentYet": False # whether the 'd' character has been send yet (gets reset on 'd DOWN', and sent when 'd' is typed from either 'UP', 'cards', or 'world' section
 }
 
 config = {
@@ -41,21 +42,26 @@ def hookCallback(event):
 
 	nameL = event.name.lower()
 
-	# Set hotkey for exiting the program
+	# SECTION 1: Set hotkey for exiting the program
 	if (nameL == "end") and config["enableQuickExit"]:
 		sys.exit()
 
-	# Record whether this key was pressed (lower case)
+	# SECTION 2: Record whether this key was pressed (lower case)
 	if event.event_type == "up":
 		gstate["down"].discard(nameL) # use discard to avoid error if not in set
 	elif event.event_type == "down":
 		gstate["down"].add(nameL)
 
 
-	# Pass through normal keys (will require keys down check later)
+	# SECTION 3: Pass through normal keys (will require keys down check later)
 	if ('d' not in gstate["down"]) or (nameL not in config["specials"]):
 		# if d is not pressed and this isn't for a d
 		if event.event_type == "down":
+			# Do 'cards' fix
+			if ('d' in gstate["down"]) and (not gstate["dSentYet"]):
+				keyboard.press('d')
+				gstate["dSentYet"] = True
+			
 			keyboard.press(event.scan_code) # scancode used to avoid issue with 'F' character (to be explicit)
 		elif event.event_type == "up":
 			keyboard.release(event.scan_code)
@@ -63,25 +69,29 @@ def hookCallback(event):
 			print("Unknown event type: " + event.event_type)
 
 
-	# Pass through 'd' based on UP event
+	# SECTION 4: Pass through 'd' based on UP event
 	if (nameL == "d"):
 		if event.event_type == "up":
-			if not gstate["viTriggeredYet"]:
+			if (not gstate["viTriggeredYet"]) and (not gstate["dSentYet"]):
 				keyboard.send('d')
+				gstate["dSentYet"] = True
 			gstate["viTriggeredYet"] = False # reset to false
 
 		elif event.event_type == "down":
-			pass # nothing goes here, alternatively we could reset viTriggeredYet=False here
+			# alternatively we could reset viTriggeredYet=False here
+			gstate["dSentYet"] = False # reset to not sent yet
 
-	# Fix "worl/world" bug
+
+	# SECTION 5: Fix "worl/world" bug
 	if any([thisVIKey in gstate["down"] for thisVIKey in config["maps"].keys()]) and (nameL == 'd' and event.event_type == 'down'):
 		# If any of the VI keys are currently pressed down, and 'd' is being PRESSED
 		keyboard.send('d') # this might only be a .press, actually; doesn't matter though
-		print("\nDid 'world' bug fix.")
+		#print("\nDid 'world' bug fix.")
+		gstate["dSentYet"] = True
 
-	# Perform VI arrow remapping
+	# SECTION 6: Perform VI arrow remapping
 	if (nameL in config["maps"].keys()) and ('d' in gstate["down"]):
-		gstate["viTriggeredYet"] = True # VI triggered, no longer type a d on release
+		gstate["viTriggeredYet"] = True # VI triggered, no longer type a 'd' on release
 		thisSend = config["maps"][nameL]
 		if event.event_type == "down":
 			keyboard.press(thisSend)
@@ -90,9 +100,9 @@ def hookCallback(event):
 		#print("\nSending: " + thisSend)
 
 
-	# Print debug info
+	# SECTION 7: Print debug info
 	if config["printDebug"]:
-		info = "\nNew Event: type({type})\tname({name})\t\tkeysDown({keysDown})) ".format(type=event.event_type, name=event.name, keysDown="|".join(gstate["down"]))
+		info = "\nNew Event: type({type})\tname({scancode} = {name})\t\tkeysDown({keysDown})) ".format(type=event.event_type, name=event.name, scancode=event.scan_code, keysDown="|".join(gstate["down"]))
 		if gstate["lastInfo"] != info:
 			print(info, end="")
 			gstate["lastInfoCount"] = 0
--- README.md
+++ README.md
@@ -18,6 +18,50 @@ Modifier keys (specifically, shift) are applied as pressed. This tool can be use
 
 ## Known Issues
 * Not all key up events are trigger properly for normal characters (ex: 'world'). This is speculative, but probably not actually an issue.
-* "carsd/cards" bug
+* 'D' presses now occur when 'd' is lifted. For video games (especially those that use WASD-keys), you'll likely want to disable this software.
+* Some keys aren't passed through, like the Windows key.
 
 If you come up with a fix, please make a pull request.
+If you notice a bug, please open an issue and/or make a pull request.
+
+## Future Ideas:
+* Consider the following mappings:
+	* b -> go to start of word
+	* w -> go to end of word
+	* 0 -> go to start of line
+
+## Refactoring Suggestions
+* Combine `gstate["viTriggeredYet"]` and `gstate["dSentYet"]` into a single `gstate["stillSendD"]` variable, that records whether or not to send the 'd'. Resets (to True) on the start of a d press, and gets flagged (to False) when a VI key is pressed, or the first time a 'd' is sent.
+
+## Documentation
+The following sections are labelled with comments in the Python code. Each section is within the `hookCallback(event)` function, and handles a certain type of key press/release occurrence.
+
+### **Section 1:** Fast exit hotkey
+By pressing the "END" key, the program is exited. Can be disabled in the `config["enableQuickExit"]`. Useful for stopping the program if any bugs occur and the keyboard is blocked.
+
+### **Section 2:** Record keys currently down
+Updates the set() at `gstate["down"]` with lowercase names of all keys currently pressed.
+
+### **Section 3:** Pass Through Normal Keys
+Passes through normal (non-VI) keys. This section is activated when 'd' is not held down, or when the key being received is not a VI key.
+
+* If the key event_type is UP (key received is being released), a release is simply sent to the computer.
+* If the key event_type is DOWN (key received is being pressed):
+	1. We check to see if 'd' is pressed down currently, and see if it has been sent for this time it is pressed.
+		* If this is the case, as would be when **typing "cards" very quickly,** (known as the 'cards' bug) send a 'd' before the received event
+	2. Finally, send the received event
+
+### **Section 4:** Pass Through 'd' on UP Event
+Normally (neglecting fast consecutive presses), the 'd' key is sent on a key up event. However, it is not sent if either 1) a VI key was pressed since it was pressed down, or 2) it was already sent because of a fast consecutive press
+
+### **Section 5:** Send 'd' after VI key (fixes 'world' bug)
+* When you type the word "world" fast, the 'l' and 'd' cause a unique case that must be caught here.
+* The `if any([...]) and <d pressed down right now>` statement checks to see if any VI keys are currently pressed, and checks to see if the current event is 'd DOWN'.
+
+### **Section 6:** Perform VI Remapping
+If the key is a mappable key, and 'd' is currently held down, send the appropriate arrowkey.
+
+### **Section 7**: Print Debug Info
+Prints debug info about the current event, and various states. In the future (or as needed), add a printout of `gstate` to the end.
+
+Sample of Debug Info: `New Event: type(down)   name(42 = shift)                keysDown(space|shift))`
