efba757c6c065924a11016fd55037435f8bfe432
ThePiGuy <ThePiGuy@users.noreply.github.com>
2020-04-30 01:43:19 -0600

    Implement framework of key tracking/typing

--- Python-Vi-ArrowKeys.py
+++ Python-Vi-ArrowKeys.py
@@ -2,35 +2,88 @@
 
 import keyboard # install with "pip install keyboard"
 
-isDDown = False
+import sys
+
+globalStates = {
+	"down": set(), # list of characters currently pressed
+	"lastInfo": "", # stores an information string printed to the user, for caching
+	"lastInfoCount": 0
+}
+
+config = {
+	"printDebug": True,
+
+	"maps": { # VI Mappings
+		"h": "left",
+		"j": "down",
+		"k": "up",
+		"l": "right"
+	}
+}
+
+config["specials"] = list(config["maps"].keys()) + ["d"] # list of all special characters to remap
+
+
 
 def hookCallback(event):
 	"""
 	Called for every key down/up event.
 	@param event a keyboard.KeyboardEvent object
+
+	Samples of event parameter (with event.to_json()):
+		{"event_type": "down", "scan_code": 30, "name": "a", "time": 1588229823.0407975, "is_keypad": false}
+		{"event_type": "up", "scan_code": 30, "name": "a", "time": 1588229823.1415234, "is_keypad": false}
+	Each attribute/key can be accessed directly with dot notation (ex: event.event_type).
 	"""
 
-	print("Got event: " + event.to_json())
+	nameL = event.name.lower()
+
+	# Print debug info
+	if config["printDebug"]:
+		info = "\nNew Event: type({type})\tname({name})\t\tkeysDown({keysDown}))".format(type=event.event_type, name=event.name, keysDown="|".join(globalStates["down"]))
+		if globalStates["lastInfo"] != info:
+			print(info, end="")
+			globalStates["lastInfoCount"] = 0
+		elif globalStates["lastInfoCount"] < 20: # only print out if it's not already been held for a while
+			print(".", end="")
+			globalStates["lastInfoCount"] += 1
+
+		globalStates["lastInfo"] = info
+
+	# Pass through normal keys (will require keys down check later)
+	if nameL not in config["specials"]:
+		if event.event_type == "down":
+			keyboard.press(event.scan_code) # scancode used to avoid issue with 'F' character (to be explicit)
+		elif event.event_type == "up":
+			keyboard.release(event.scan_code)
+		else:
+			print("Unknown event type: " + event.event_type)
+
+
+	# Record whether this key was pressed (lower case)
+	if event.event_type == "up":
+		globalStates["down"].discard(nameL) # use discard to avoid error if not in list
+	elif event.event_type == "down":
+		globalStates["down"].add(nameL)
+
+
+	# Perform VI arrow remapping
+	if (nameL in config["maps"].keys()) and event.event_type == "down" and ('d' in globalStates["down"]):
+		thisSend = config["maps"][nameL]
+		if "shift" in globalStates["down"]:
+			# holding shift
+			thisSend += "+shift"
+		keyboard.send(thisSend)
 
 
 
-def dDownCallback():
-	isDDown = True
-
-def dUpCallback():
-	isDDown = False
+	# Set hotkey for exiting the program
+	if (nameL == "end"):
+		sys.exit()
 
 
 
-
-
-keyboard.on_press_key('d', dDownCallback)
-keyboard.on_release_key('d', dUpCallback)
-
-keyboard.hook(hookCallback)
-
-
-
+keyboard.hook(hookCallback, True) # supress characters
 
 # wait forever
 keyboard.wait()
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AHK-Vi-ArrowKeys
-An AutoHotkey script that works like Karabiner for Mac's VI mode (but for Windows, now). Press and hold "D", and use the right hand home row to emulate arrow keys, based on the VI mapping.
+An ~AutoHotkey~ Python script that works like Karabiner for Mac's VI mode (but for Windows, now). Press and hold "D", and use the right hand home row to emulate arrow keys, based on the VI mapping.
+
+This project was originally attempted in AHK, but switched to Python for more flexibility.
 
 In a previous version of Karabiner for Mac, this mode was triggered by pressing S+D. However, now it is triggered by only pressing D (as per the most recent version of the Mac software).
 
@@ -15,7 +17,6 @@ If you don't press any of those before releasing the D key, a "d" is typed.
 Modifier keys (specifically, shift) are applied as pressed. This tool can be used to move around, or select text.
 
 ## Known Issues
-* Characters following a letter 'd' may be swapped from how you intended them. Example, type the word "did" a bunch of times with spaces between them.
-	* This is partially fixed in the `Else` block right before the VI key remapping. However, this fix doesn't really work that well.
+* None yet, still developing
 
 If you come up with a fix, please make a pull request.
